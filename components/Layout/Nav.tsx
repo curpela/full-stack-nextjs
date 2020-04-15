567481daa5d4f1efc5a509cb4b84da9be9a60fe2
Anthony Venturini <anthonydventurini@gmail.com>
2020-04-14 17:04:09 -0700

    deploying to heroku x9

--- components/Layout/Nav.tsx
+++ components/Layout/Nav.tsx
@@ -3,24 +3,28 @@ import Link from "next/link";
 import CurpelaWordLogo from "../Sharable/CurpelaWordLogo";
 import "./Nav.scss";
 
-
-
 const Nav: React.FC = () => {
   return (
     <div className="nav">
-      <div className="left-nav">
+      <div className="nav-content">
+        <div className="left-nav">
           <a href="/">
-            <CurpelaWordLogo fill="white" width="110" style={{marginTop: "10px"}}/>
+            <CurpelaWordLogo
+              fill="white"
+              width="110"
+              style={{ marginTop: "10px" }}
+            />
           </a>
-      </div>
+        </div>
 
-      <div className="right-nav">
-        <Link href="/login" >
-          <button>Log In</button>
-        </Link>
-        <Link href="/signup" >
-          <button className="signup">Sign Up</button>
-        </Link>
+        <div className="right-nav">
+          <Link href="/login">
+            <button>Log In</button>
+          </Link>
+          <Link href="/signup">
+            <button className="signup">Sign Up</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
